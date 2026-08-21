6d1b988844836642759611b7d24e2428ec538e59
kerrzo80-cpu <kerrzo80@icloud.com>
2026-08-21 08:00:28 +0100

    Register Blake quote and job operator capabilities

--- apps/web/src/lib/blake-core/index.ts
+++ apps/web/src/lib/blake-core/index.ts
@@ -1,9 +1,14 @@
 import { coreCapabilities } from "./capabilities";
+import { operatorCapabilities } from "./operator-capabilities";
 import { createBlakeCapabilityRegistry } from "./registry";
 
 export * from "./context-store";
 export * from "./types";
 export { createBlakeCapabilityRegistry } from "./registry";
 export { coreCapabilities } from "./capabilities";
+export { operatorCapabilities } from "./operator-capabilities";
 
-export const blakeCore = createBlakeCapabilityRegistry(coreCapabilities);
+export const blakeCore = createBlakeCapabilityRegistry([
+  ...coreCapabilities,
+  ...operatorCapabilities,
+]);
